bff9b13fe784ece43bda1f3e9fbbb0a31b49889b
hoesung ryu <46211123+hoesung@users.noreply.github.com>
2018-12-28 15:31:24 +0900

    Update Exercise3.py

--- hoe sung ryu/DEC 28 2018/Exercise3.py
+++ hoe sung ryu/DEC 28 2018/Exercise3.py
@@ -1,8 +1,11 @@
-# Exercise 3-1
+#Excercis3-1
 def Printing_Quotes():
-    quotes = {'What is the quote?': 'These are not the droids you are looking for.', 'Who said it?' : 'a','a':'b'}
-    sentence=input('Put your sentence for quotes')
-    print(quotes['sentence'])
+    quotes = {"Simone de Beauvoir": "Change your life today. Don't gamble on the future, act now, without delay.",
+              "Og Mandino": "Failure will never overtake me if my determination to succeed is strong enough."}
+    author=input('What is author?')
+    print(quotes[author])
+
+#Excercise3-2
 
 # Printing_Quotes()
 

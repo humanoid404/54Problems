22ae1bad9d2b237aeb0633727990adb97d4babdd
hoesung ryu <46211123+hoesung@users.noreply.github.com>
2018-12-28 16:24:56 +0900

    Update Exercise8.py

--- hoe sung ryu/DEC 28 2018/Exercise8.py
+++ hoe sung ryu/DEC 28 2018/Exercise8.py
@@ -1,11 +1,17 @@
 #Exercise8-1
 
+def pizza():
+    ppl=int(input('How many people?'))
+    pizzas=int(input("how many pizzas do you have"))
+    each=int(input("How many pieces each person need to eat?"))
+    num_p = 8 * pizzas
+    left_p = num_p % int(ppl * each)
+    if left_p ==0:
+        print('{0} people with {1} pizzas'.format(ppl,pizzas))
+        print('Each person gets %d pieces of pizza.' %each)
+        print('There are %d leftover pieces' %left_p)
+    else:
+        need_pizza=pizzas+1
+        print('we need to but more pizza i.e. {} pizzas '.format(need_pizza) )
 
-ppl=int(input('How many people?'))
-pizzas=int(input("how many pizzas do you have"))
-each=int(input("How many pieces each person eat?"))
-num_p=8*pizzas
-left_p=num_p%int(ppl*each)
-print('{0} people with {1} pizzas'.format(ppl,pizzas))
-print('Each person gets %d pieces of pizza.' %each)
-print('There are %d leftover pieces' %left_p)
+pizza()

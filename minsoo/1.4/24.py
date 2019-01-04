528d33494006f9fdfd123bc7e714c85b4b3b5f62
wwnis7 <46211067+wwnis7@users.noreply.github.com>
2019-01-04 16:20:10 +0900

    Update 24.py

--- minsoo/1.4/24.py
+++ minsoo/1.4/24.py
@@ -1,8 +1,23 @@
 print("Enter two strings and I'll tell you if they are anagrams: ")
 def isAnagram(a,b):
-    if len(f'{a}') == len(f'{b}'):
-        result = ''
+    if len(a) == len(b):
+        A = ''
+        for i in range(len(a)):
+            for j in range(len(b)):
+                try:
+                    if a[i] == b[j]:
+                        A = A + a[i]
+                except:
+                    pass
+        print(A)
+        print(a)
+        if len(A) == len(C):
+            result = ''
+        else:
+            result = 'not '
     else:
         result = 'not '
     print(f'"{a}" and "{b}" are {result}anagrams')
-isAnagram(input('Enter the first string: '), input('Enter the second string: '))
+a = input('Enter the first string: ')
+b = input('Enter the second string: ')
+isAnagram(a, b)

88e63efa091e29baae2a90b6ddc3d5dc86eb6b5a
persontime <persontime@LinuxAcer.Mint>
2019-01-04 16:01:13 +0900

    fixed 24.py

--- youngchan/24.py
+++ youngchan/24.py
@@ -1,16 +1,38 @@
+# -1=different length, 0=same length not anagram, 1=anagram
 def isAnagram(word1, word2):
-    isAnagramFlag = True
-    if len(word1) == len(word2):
-        for i in range(len(word1)):
-            if word1[i] != word2[-1-i]:
-                isAnagramFlag = False
+    word1 = word1.lower().strip()
+    word2 = word2.lower().strip()
+    if len(word1) != len(word2):
+        return -1
+    l = len(word1)  # word length
+
+    d = {}  # dictionary for recording the index per word
+    # d=={"c":[[1,3], [2,4]], ...} will mean that "c" appears at the 1st, 3rd index in word1
+    #                                         and "c" appears at the 2nd, 4th index in word2
+    for i1 in range(l):
+        if word1[i1] not in d:
+            d[word1[i1]] = [[i1],[]]
+        else:
+            d[word1[i1]][0].append(i1)
+    for i2 in range(l):
+        if word2[i2] not in d:
+            d[word2[i2]] = [[],[i2]]
+        else:
+            d[word2[i2]][1].append(i2)
+    for k in d:
+        if len(d[k][0]) != len(d[k][1]):
+            return 0
+    return 1
+
+
+while True:
+    print("Enter two strings and I'll tell you if they are anagrams.")
+    word1 = input("Enter the first string: ").strip()
+    word2 = input("Enter the second string: ").strip()
+    isAnagramFlag = isAnagram(word1, word2)
+    if isAnagramFlag < 0:
+        print('"{}" and "{}" are not anagrams because they have different lengths.\n'.format(word1, word2))
+    elif isAnagramFlag == 0:
+        print('"{}" and "{}" are not anagrams.\n'.format(word1, word2))
     else:
-        isAnagramFlag = False
-    return isAnagramFlag
-
-
-print("Enter two strings and I'll tell you if they are anagrams.")
-word1 = input("Enter the first string: ").strip()
-word2 = input("Enter the second string: ").strip()
-print("\"{}\" and \"{}\" are {}anagrams.".format(word1, word2,
-                                                 "" if isAnagram(word1, word2) else "not "))
+        print('"{}" and "{}" are anagrams.\n'.format(word1, word2))

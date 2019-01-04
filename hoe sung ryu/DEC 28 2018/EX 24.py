11604affcec49a087b982d6ef3edc55db49d8163
hoesung ryu <46211123+hoesung@users.noreply.github.com>
2019-01-04 16:21:31 +0900

    Add files via upload

--- hoe sung ryu/DEC 28 2018/EX 24.py
+++ hoe sung ryu/DEC 28 2018/EX 24.py
@@ -2,30 +2,27 @@
 def isAnagram(x ,y):
     x_list =list(x)
     y_list =list(y)
-
     #value=잘못된 문자(i)와 key=위치(n) dictionary
     w_dic={}
     if len(x_list)==len(y_list):
         n = -1 # find wrong position
-        T = [] # 맞는게 몇개인가?
+        T = 0 # 맞는게 몇개인가
         w_dic = {}
         for i in x_list:
             n += 1
             if i in y_list:
-                T.append(1)
+                T+=1
+                tt=y_list.index(i)
+                del(y_list[tt])
             else:
-                T.append(0)
+                T+=0
                 w_dic[n] = i
-
-        print(w_dic)
-        print(T)
-
-        if sum(T) == len(x):
+        if  T== len(x):
             print("is aAnagam")
+            print(w_dic)
         else:
             print("is not Anaglam")
             print(w_dic)
-            # print("because #{order} of second word has not the same vlaue as an {value}".format(order=,value=))
     else:
         print("It is not Anagram because of length")
 

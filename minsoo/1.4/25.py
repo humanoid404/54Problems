45558da87f8d8b69cfc39323352d54908fe9f88d
wwnis7 <46211067+wwnis7@users.noreply.github.com>
2019-01-06 19:15:55 +0900

    Update 25.py

--- minsoo/1.4/25.py
+++ minsoo/1.4/25.py
@@ -1,17 +1,15 @@
 D = {1:'a very weak', 2:'a weak', 3:'a strong', 4:'a very strong', 0:'untyped'}
-
+x = input('Enter your password')
 def passwordValidator():
     if len(x) < 8 and x.isnumeric():
         return 1
     elif len(x) < 8 and x.isalpha():
         return 2
-    elif len(x) >= 8 and x.isalnum() and x.isalpha() == 0 and x.isnumeric() == 0:
+    elif len(x) >= 8 and x.isalnum() and not x.isalpha() and not x.isnumeric():
         return 3
-    elif len(x) >= 8 and x.isalnum() == 0:
+    elif len(x) >= 8 and not x.isalnum():
         return 4
     else:
         return 0
-
-x = input('Enter your password')
 passwordValidator()
 print(f"The password '{x}' is {D[passwordValidator()]} password.")

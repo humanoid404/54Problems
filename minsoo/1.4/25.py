24ab8ea9da6c877c962751d7fcd1a6673af84b8f
wwnis7 <46211067+wwnis7@users.noreply.github.com>
2019-01-06 18:59:45 +0900

    Update 25.py

--- minsoo/1.4/25.py
+++ minsoo/1.4/25.py
@@ -1,17 +1,17 @@
 D = {1:'a very weak', 2:'a weak', 3:'a strong', 4:'a very strong', 0:'untyped'}
 
-def passwordValidator(x):
-    if f'{x}'.isdigit and len(f'{x}') < 8:
+def passwordValidator():
+    if len(x) < 8 and x.isnumeric():
         return 1
-    elif f'{x}'.isalpha() and len(f'{x}') < 8:
+    elif len(x) < 8 and x.isalpha():
         return 2
-    elif f'{x}'.isalnum() and f'{x}'.isalpha() == 0 and f'{x}'.isdigit() == 0 and len(f'{x}') >= 8:
+    elif len(x) >= 8 and x.isalnum() and x.isalpha() == 0 and x.isnumeric() == 0:
         return 3
-    elif f'{x}'.isalnum() == 0 and len(f'{x}') >= 8:
+    elif len(x) >= 8 and x.isalnum() == 0:
         return 4
     else:
         return 0
 
 x = input('Enter your password')
-passwordValidator(x)
-print(f"The password '{x}' is {D[passwordValidator(x)]} password.")
+passwordValidator()
+print(f"The password '{x}' is {D[passwordValidator()]} password.")

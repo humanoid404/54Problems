2f3d5c521b604b953a4495484628db65144402f8
wwnis7 <46211067+wwnis7@users.noreply.github.com>
2019-01-11 23:36:03 +0900

    Update 44.py

--- minsoo/1.11/44.py
+++ minsoo/1.11/44.py
@@ -4,14 +4,13 @@ from pprint import pprint
 json_data = open('C:/Users/wwnis/Desktop/44.JSON').read()
 data = json.loads(json_data)
 pprint(data)
-a = 0
-while a != True:
+while True:
     a = input('What is the product name? ')
     for i in range(len(data["products"])):
         if data["products"][i]["name"] == a:
-            a = True
             print(f'Name: {data["products"][i]["name"]}')
             print(f'Price: ${data["products"][i]["price"]}')
             print(f'Quantity on hand: {data["products"][i]["quantity"]}')
             break
-    if a != True: print('Sorry, that product was not found in our inventory.')
+    if data["products"][i]["name"] == a: break
+    else: print('Sorry, that product was not found in our inventory.')

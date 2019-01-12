13cff1f40e45300c8b0e656d770882afdc466857
wwnis7 <46211067+wwnis7@users.noreply.github.com>
2019-01-12 18:36:19 +0900

    Update 46.py

--- minsoo/1.11/46.py
+++ minsoo/1.11/46.py
@@ -18,12 +18,9 @@ while i in range(len(A)):
         while A[i] == A[i+1]:
             j += 1
             A.pop(i+1)
-    except: break                               # i == len(A) - 1 일때만 except에 걸린다. 주의, A[i] != A[i+1]과 A[i] == A[i+1]이 '오류'인 경우는 다르다. (즉, 마지막에만 except가 걸리기 때문에, ''를 마지막에 추가해줌.)
+    except: break                               # i == len(A) - 1 일때만 except에 걸린다. 주의, A[i] != A[i+1]인 경우와 A[i] == A[i+1]이 '오류'인 경우는 다르다. (즉, 마지막에만 except가 걸리기 때문에, ''를 마지막에 추가해줌.)
     B.append([j, A[i]])
     i += 1
-def getKey(item):
-    return[item[0]]
-C = sorted(B, key = getKey)
-C.reverse()
+C = sorted(B, key = lambda x: x[0], reverse = True)
 for i in range(len(C)):
     print(f'{C[i][1]+":":<15} {"*"*C[i][0]:<10}')
